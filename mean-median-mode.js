0649b1bd43f481d33a661a2d96c138b296438b8f
Angelo Altavas <angeloaltavas@yahoo.com>
2014-08-18 09:15:16 -0700

    made corrections

--- mean-median-mode.js
+++ mean-median-mode.js
@@ -1,6 +1,5 @@
-var numbers = process.argv
-numbers.shift();
-numbers.shift();
+var numbers = process.argv;
+numbers.splice(0,2);
 
 function mean (numbers) {
 	var total = 0;
@@ -17,25 +16,26 @@ function median (numbers) {
 	numbers.sort();
 	if(numbers.length % 2 === 0) {
 		median = (parseFloat(numbers[numbers.length / 2 - 1]) + parseFloat(numbers[numbers.length / 2])) / 2;
-	} else {
+	} 
+	else {
 		median = parseFloat(numbers[(numbers.length - 1) / 2]);
 	}
-
 	console.log("The median is " + median);
 }
 
 function modes (numbers) {
-	var counter = {};
+	var counter = [];
     var mode = [];
     var max = 0;
     for (var i in numbers) {
-        if (!(numbers[i] in counter))
+        if (counter[numbers[i]] === undefined)
             counter[numbers[i]] = 0;
         counter[numbers[i]]++;
  
-        if (counter[numbers[i]] == max) 
+        if (counter[numbers[i]] == max) { 
             mode.push(numbers[i]);
-        else if (counter[numbers[i]] > max) {
+        }
+        if (counter[numbers[i]] > max) {
             max = counter[numbers[i]];
             mode = [numbers[i]];
         }
